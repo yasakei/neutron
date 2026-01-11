7aa0b1660baa0f8a3b9c84a6f08acbfdda5df236
yasakei <yas.studios.en@gmail.com>
2026-01-11 22:56:12 +0600

    windows build error

--- include/cross-platfrom/compat_unistd.h
+++ include/cross-platfrom/compat_unistd.h
@@ -2,13 +2,12 @@
 
 #ifdef _WIN32
   #include <io.h>       // for _access
-  #include <process.h>  // for _getpid
-  #include <windows.h>  // for Sleep
+  #include <windows.h>  // for Sleep and GetCurrentProcessId
   #include <direct.h>   // for _chdir, _mkdir
   #include <stdlib.h>
 
   #define access _access
-  #define getpid _getpid
+  #define getpid() GetCurrentProcessId()
   #define sleep(x) Sleep((x) * 1000)
   #define usleep(x) Sleep((x) / 1000)
   #define F_OK 0
