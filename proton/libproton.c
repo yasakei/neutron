50db3339cc93636dd58b1b1e64b413a22af3c5fd
yasakei <yasakei@proton.me>
2026-05-29 16:51:15 +0600

    fix: ABI mismatch in MacOS ARM

--- proton/libproton.c
+++ proton/libproton.c
@@ -14,6 +14,22 @@ char debug['Z'+1] = {0};
 
 static FILE *g_outf;
 
+static Target
+proton_host_target(void)
+{
+#if defined(__APPLE__) && (defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64))
+	return T_arm64_apple;
+#elif defined(__APPLE__) && (defined(__x86_64__) || defined(_M_X64) || defined(__amd64__))
+	return T_amd64_apple;
+#elif defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64)
+	return T_arm64;
+#elif defined(__riscv) && (__riscv_xlen == 64)
+	return T_rv64;
+#else
+	return T_amd64_sysv;
+#endif
+}
+
 static void
 data_cb(Dat *d)
 {
@@ -98,7 +114,7 @@ proton_compile_ssa(const char *ssa, FILE *outf)
 	FILE *inf;
 
 	g_outf = outf;
-	T = Deftgt;
+	T = proton_host_target();
 
 	inf = fmemopen((void *)ssa, strlen(ssa), "r");
 	if (!inf)
